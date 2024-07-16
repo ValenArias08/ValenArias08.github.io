1916fc70e2165d32f385043ff55ed743dc89da8d
ValenArias08 <valentina.ariasv20@gmail.com>
2024-07-15 22:52:00 -0500

    Se agregan imagenes relacionadas a los proyectos y se ajusta el esapcio del iframe para que no aparezca duplicada la barra deslizadora

--- JS/funciones.js
+++ JS/funciones.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
     // Cargar el menú desde menu.html
     $('#menu-container').load('Pages/menu.html');
 });
+
+
+function loadPage(url) {
+    const iframe = document.getElementById('content-frame');
+    iframe.src = url;
+
+    iframe.onload = function() {
+        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+    };
+}
 /*
 document.addEventListener('DOMContentLoaded', function () {
     const imageContainer = document.querySelector('.image-container');
